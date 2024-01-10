a56539773960d783e8c8c4bb441866093f637106
charlie4284 <yangsoo.yoon@canonical.com>
2024-01-10 09:23:04 +0800

    test: wait for id_rsa

--- tests/integration/pre_run_script.sh
+++ tests/integration/pre_run_script.sh
@@ -9,4 +9,22 @@
 # The runner has ~/.ssh dir owned by Docker, change the ownership for ssh access.
 sudo mkdir -p ~/.ssh
 sudo chown -R runner:runner ~/.ssh
-sudo juju bootstrap localhost
+
+wait_time=5
+max_attempts=10
+attempt=1
+
+while [[ $attempt -le $max_attempts ]]; do
+  if [[ -f "$~/.ssh/id_rsa" ]]; then
+    echo "id_rsa found!"
+    break
+  fi
+
+  echo "id_rsa not found. Attempt $attempt of $max_attempts. Waiting for $wait_time seconds..."
+  sleep $wait_time
+  attempt=$((attempt + 1))
+done
+
+if [[ $attempt -gt $max_attempts ]]; then
+  echo "Timeout. id_rsa not found after $((max_attempts * wait_time)) seconds."
+fi
