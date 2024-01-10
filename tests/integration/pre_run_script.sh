6534ffa3148971c13f62e43d963a6cf006d50038
charlie4284 <yangsoo.yoon@canonical.com>
2024-01-10 10:25:28 +0800

    test: copy id_rsa files

--- tests/integration/pre_run_script.sh
+++ tests/integration/pre_run_script.sh
@@ -9,22 +9,5 @@
 # The runner has ~/.ssh dir owned by Docker, change the ownership for ssh access.
 sudo mkdir -p ~/.ssh
 sudo chown -R runner:runner ~/.ssh
-
-wait_time=5
-max_attempts=10
-attempt=1
-
-while [[ $attempt -le $max_attempts ]]; do
-  if [[ -f "$~/.ssh/id_rsa" ]]; then
-    echo "id_rsa found!"
-    break
-  fi
-
-  echo "id_rsa not found. Attempt $attempt of $max_attempts. Waiting for $wait_time seconds..."
-  sleep $wait_time
-  attempt=$((attempt + 1))
-done
-
-if [[ $attempt -gt $max_attempts ]]; then
-  echo "Timeout. id_rsa not found after $((max_attempts * wait_time)) seconds."
-fi
+cp ~/.local/share/juju/ssh/juju_id_rsa ~/.ssh/id_rsa
+cp ~/.local/share/juju/ssh/juju_id_rsa.pub ~/.ssh/id_rsa.pub
